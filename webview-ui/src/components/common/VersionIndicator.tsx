9075812b1fd9dccd175261e31cc0ddc3b872c5cc
evhenious <evhenious@gmail.com>
2026-04-26 16:20:00 +0200

    some tiny renamings

--- webview-ui/src/components/common/VersionIndicator.tsx
+++ webview-ui/src/components/common/VersionIndicator.tsx
@@ -15,7 +15,7 @@ const VersionIndicator: React.FC<VersionIndicatorProps> = ({ onClick, className 
 			onClick={onClick}
 			className={`text-xs text-vscode-descriptionForeground rounded-full hover:text-vscode-foreground transition-colors cursor-pointer px-2 py-1 border ${className}`}
 			aria-label={t("chat:versionIndicator.ariaLabel", { version: Package.version })}>
-			v{Package.version}
+			based on v{Package.version}
 		</button>
 	)
 }
